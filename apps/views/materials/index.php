24522a9ae34b37d4af723827e63a8eea4bb97393
Wildcorsair <wildcorsar@rambler.ru>
2014-04-28 17:26:21 +0300

    Update 28.04.2014

--- apps/views/materials/index.php
+++ apps/views/materials/index.php
@@ -1,5 +1,8 @@
 <div id="content-block">
 	<div class="content-item-block">
-		Это content-item-block.
+		<h3>Настройка модемов ZTE H108L Annex B</h3>
+		<div class="article-info">
+			<span>15.04.2014</span><span>Размесил</span>
+		</div>
 	</div>
 </div>
