1e50cddd8be8c5588176c537f77380d87bc1b319
Wildcorsair <wildcorsar@rambler.ru>
2014-04-29 16:15:34 +0300

    Commit 29.04.2014

--- apps/views/materials/index.php
+++ apps/views/materials/index.php
@@ -1,8 +1,9 @@
 <div id="content-block">
 	<div class="content-item-block">
 		<h3>Настройка модемов ZTE H108L Annex B</h3>
-		<div class="article-info">
-			<span>15.04.2014</span><span>Размесил</span>
+		<div class="material-info">
+			<div class="article-date-ico"></div><span>15.04.2014</span>
+			<span class="">Размесил: </span><span>Администратор</span>
 		</div>
 	</div>
 </div>
