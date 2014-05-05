150282614c95ca99efd89621cec1b5bfdf2d60fc
Home <dark-edge@mail.ru>
2014-05-05 23:11:31 +0300

    Article list design

--- apps/views/materials/index.php
+++ apps/views/materials/index.php
@@ -2,8 +2,17 @@
 	<div class="content-item-block">
 		<h3>Настройка модемов ZTE H108L Annex B</h3>
 		<div class="material-info">
-			<div class="article-date-ico"></div><span>15.04.2014</span>
-			<span class="">Размесил: </span><span>Администратор</span>
+			<table>
+				<tbody>
+					<tr>
+						<td><div class="article-date-ico"></div></td>
+						<td>15.04.2014</td>
+						<td><span class="">Разместил: </span></td>
+						<td>Администратор</td>
+					</tr>	
+				</tbody>
+			</table>
+			<div class="article-preview-image"></div>
 		</div>
 	</div>
 </div>
